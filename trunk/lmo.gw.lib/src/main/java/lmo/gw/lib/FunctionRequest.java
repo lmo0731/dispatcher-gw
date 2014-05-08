8b6d3932a836dca29ab4c1cfe121c497c82bc45d
lmo0731 <lmo0731@3b3340a2-8003-39a5-da9f-b674f6b5a0d1>
2014-05-08 09:42:30 +0000

    function headers can get by name, case insensetive

--- trunk/lmo.gw.lib/src/main/java/lmo/gw/lib/FunctionRequest.java
+++ trunk/lmo.gw.lib/src/main/java/lmo/gw/lib/FunctionRequest.java
@@ -22,7 +22,23 @@ public class FunctionRequest<T> {
     private T requestObject;
     private ArrayList<String> pathParams;
     private Map<String, String[]> queryParams;
-    private Map<String, Enumeration<String>> headers = new HashMap<String, Enumeration<String>>();
+    private Map<String, Enumeration<String>> headers = new HashMap<String, Enumeration<String>>() {
+        @Override
+        public Enumeration<String> get(Object key) {
+            if (key != null && key instanceof String) {
+                key = key.toString().toLowerCase();
+            }
+            return super.get(key); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Enumeration<String> put(String key, Enumeration<String> value) {
+            if (key != null) {
+                key = key.toLowerCase();
+            }
+            return super.put(key, value); //To change body of generated methods, choose Tools | Templates.
+        }
+    };
     private String functionName;
 
     public FunctionRequest(Logger logger, String functionName, T requestObject, Map<String, String[]> params) {
