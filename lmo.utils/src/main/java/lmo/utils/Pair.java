ed338c36459129b3245a7908be2aa890cf8aa4c7
lmo0731 <lmo0731@3b3340a2-8003-39a5-da9f-b674f6b5a0d1>
2014-05-10 09:46:02 +0000

    Pair type added

---
+++ lmo.utils/src/main/java/lmo/utils/Pair.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lmo.utils;
+
+/**
+ *
+ * @author munkhochir <munkhochir@mobicom.mn>
+ */
+public class Pair<F, S> {
+
+    public F first;
+    public S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s)", first.toString(), second.toString());
+    }
+}
