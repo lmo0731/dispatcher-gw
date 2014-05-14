ada8d7ddfebe2bbbd3cc43f1d90612c01807c254
lmo0731 <lmo0731@3b3340a2-8003-39a5-da9f-b674f6b5a0d1>
2014-05-14 03:08:16 +0000

    error handler added

---
+++ trunk/lmo.gw.dispatcher.lib/src/main/java/lmo/gw/dispatcher/lib/impl/DefaultErrorHandler.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lmo.gw.dispatcher.lib.impl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
+import lmo.gw.dispatcher.lib.DispatcherException;
+import lmo.gw.dispatcher.lib.ErrorHandler;
+import lmo.gw.lib.Function;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author munkhochir <munkhochir@mobicom.mn>
+ */
+public class DefaultErrorHandler extends ErrorHandler {
+
+    @Override
+    public void handle(HttpServletResponse response, Logger logger, DispatcherException ex) throws IOException {
+        String res = ex.getMessage();;
+        PrintWriter out = response.getWriter();
+        try {
+            response.setContentType(Function.TEXT_PLAIN);
+            out.println(res);
+        } finally {
+            out.close();
+        }
+    }
+}
