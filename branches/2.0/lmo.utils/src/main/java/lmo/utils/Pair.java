117f2da69f28e5e1ced77314424ba12c45512977
lmo0731 <lmo0731@3b3340a2-8003-39a5-da9f-b674f6b5a0d1>
2014-09-29 10:47:46 +0000

    pair update

--- branches/2.0/lmo.utils/src/main/java/lmo/utils/Pair.java
+++ branches/2.0/lmo.utils/src/main/java/lmo/utils/Pair.java
@@ -7,17 +7,35 @@ package lmo.utils;
 /**
  *
  * @author munkhochir <munkhochir@mobicom.mn>
+ * @param <F>
+ * @param <S>
  */
 public class Pair<F, S> {
 
-    public F first;
-    public S second;
+    protected F first;
+    protected S second;
 
     public Pair() {
     }
 
     public Pair(F first, S second) {
         this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public void setSecond(S second) {
         this.second = second;
     }
 
